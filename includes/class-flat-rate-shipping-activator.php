1a76e23fdec0c5286772076709f227ac490ae0ec
Juan Granja <ggjuanb@hotmail.com>
2024-03-17 17:51:05 -0500

    changes

--- includes/class-flat-rate-shipping-activator.php
+++ includes/class-flat-rate-shipping-activator.php
@@ -30,7 +30,27 @@ class Flat_Rate_Shipping_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		global $wpdb;
 
+		$table_name = $wpdb->prefix . 'custom_cities';
+		
+		// Check if the table already exists
+		if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+		
+			$sql = "CREATE TABLE $table_name (
+				id INT NOT NULL AUTO_INCREMENT,
+				cityId VARCHAR(255) NOT NULL,
+				countryCode VARCHAR(4) NOT NULL,
+				stateCode VARCHAR(4) NOT NULL,
+				price DECIMAL(10,2) NOT NULL,
+				PRIMARY KEY (id),
+				FOREIGN KEY (stateCode) REFERENCES states(stateCode),
+				FOREIGN KEY (cityId) REFERENCES cities(cityId)
+			)";
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+			dbDelta($sql);
+		}
+		
 	}
 
 }
